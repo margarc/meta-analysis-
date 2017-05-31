7c57ef5f7d55e951733f9dc18e96d5464bfe23cf
Margarita <margarita.cariolou@gmail.com>
2017-05-31 03:20:08 +0300

    Update margarita_latest_vdch.R

--- margarita_latest_vdch.R
+++ margarita_latest_vdch.R
@@ -1,6 +1,6 @@
 getwd()
 setwd("C:/Users/INSPIRON/Documents/Rch")
-#############MAIN ANALYSES###################
+####MAIN ANALYSES
 #prevalence
 prevalencedata <- read.csv("prevalence.rda", as.is=TRUE)
 #type: prevalencedata to view the data
@@ -18,26 +18,46 @@ forest(mprev1, comb.fixed=FALSE, xlab= "proportion")
 #repeat to get the fixed effect model
 ######################################################
 forest(mprev1, comb.random=FALSE, xlab= "proportion")
-##############
-##############
+####
+####
 #now produce the funnel plot:
 funnel(mprev1)
-##############################
-##############################
+####
+####
 #Subgroup analyseis: children with sepsis
-##############################
+####
 prevsepsis <- read.csv("withsepsis.rda", as.is=TRUE)
 prevsepsis
 metaprop(vddseps, totseps, studlab=paste(study), data=prevsepsis)
 mprevsepsis1 <- metaprop(vddseps, totseps, studlab=paste(study), data=prevsepsis)
 mprevsepsis1
-###############################
+####
 #get forest plot fixed effect model:
 forest(mprevsepsis1, comb.random=FALSE, xlab= "proportion")
 #get random effects model forest plot:
 forest(mprevsepsis1, comb.fixed=FALSE, xlab= "proportion")
 funnel(mprevsepsis1)
-################################
-################################
+####
+####
 #MORTALITY
-#open file: mortadata.rda
+#open file: mortaldata.rda
+mortality <- read.csv("mortaldata.rda", as.is=TRUE)
+mortality
+metabin(Eedeaddef, Nealldef, Ecdeadnodef, Ncallnondef, sm= "OR", method="I", data=mortality, studlab=study)
+#check the above cz random&fixed give same results
+##################################
+metamortal <- metabin(Eedeaddef, Nealldef, Ecdeadnodef, Ncallnondef, sm= "OR", method="I", data=mortality, studlab=study)
+metamortal
+forest(metamortal, comb.fixed=FALSE, xlab= "odds ratio")
+#yes why xlab= proportion in the other analysis? clarify
+#same to get fixed effect model result:
+forest(metamortal, comb.random=FALSE, xlab= "odds ratio")
+#get funnel plot
+funnel(metamortal)
+#to do:
+####
+#senstivity analysis: high qualiy studies only prevalence
+#sens analysis for prevalence in those 21 studies that reported vdd under our set threshold <20ng/ml
+#sample size of study >=150 versus <150 
+#cont here
+
