fc3649102cbc46d7b2aa0c22b494c91126c4119e
Margarita <margarita.cariolou@gmail.com>
2017-05-31 15:37:25 +0100

    Update margarita_latest_vdch.R

--- margarita_latest_vdch.R
+++ margarita_latest_vdch.R
@@ -1,66 +1,69 @@
 getwd()
 setwd("C:/Users/INSPIRON/Documents/Rch")
-####MAIN ANALYSES
-#prevalence
+# MAIN ANALYSES
+# prevalence
 prevalencedata <- read.csv("prevalence.rda", as.is=TRUE)
 #type: prevalencedata to view the data
 prevalencedata
 library(meta)
 #library(meta) will load the package
 metaprop(vddch, totch, studlab=paste(study), data = prevalencedata)
-#name this: mprev1
+# write the results
+#
+#
+# name this: mprev1
 mprev1 <- metaprop(vddch, totch, studlab=paste(study), data = prevalencedata)
-#test it below
+# test it below
 mprev1
-#for the forest plot:
+# for the forest plot:
 forest(mprev1, comb.fixed=FALSE, xlab= "proportion")
-#the above for the random effects model
-#repeat to get the fixed effect model
-######################################################
+# the above for the random effects model
+# repeat to get the fixed effect model
+###
 forest(mprev1, comb.random=FALSE, xlab= "proportion")
-####
-####
+###
+###
 #now produce the funnel plot:
 funnel(mprev1)
-####
-####
-#Subgroup analyseis: children with sepsis
-####
+###
+###
+# Subgroup analyseis: children with sepsis
+###
 prevsepsis <- read.csv("withsepsis.rda", as.is=TRUE)
 prevsepsis
 metaprop(vddseps, totseps, studlab=paste(study), data=prevsepsis)
 mprevsepsis1 <- metaprop(vddseps, totseps, studlab=paste(study), data=prevsepsis)
 mprevsepsis1
-####
+###
 #get forest plot fixed effect model:
 forest(mprevsepsis1, comb.random=FALSE, xlab= "proportion")
 #get random effects model forest plot:
 forest(mprevsepsis1, comb.fixed=FALSE, xlab= "proportion")
 funnel(mprevsepsis1)
-####
-####
-#MORTALITY
-#open file: mortaldata.rda
+###
+###
+#  MORTALITY
+# open file: mortaldata.rda
 mortality <- read.csv("mortaldata.rda", as.is=TRUE)
 mortality
 metabin(Eedeaddef, Nealldef, Ecdeadnodef, Ncallnondef, sm= "OR", method="I", data=mortality, studlab=study)
-#check the above cz random&fixed give same results
-##################################
+# check the above cz random&fixed give same results
+###
 metamortal <- metabin(Eedeaddef, Nealldef, Ecdeadnodef, Ncallnondef, sm= "OR", method="I", data=mortality, studlab=study)
 metamortal
 forest(metamortal, comb.fixed=FALSE, xlab= "odds ratio")
-#yes why xlab= proportion in the other analysis? clarify
-#same to get fixed effect model result:
+# yes why xlab= proportion in the other analysis? clarify
+# same to get fixed effect model result:
 forest(metamortal, comb.random=FALSE, xlab= "odds ratio")
-#get funnel plot
+# get funnel plot
 funnel(metamortal)
-#to do: 
-#the following ensitivity analyses 
-#for prevalence of: high qualiy studies only (>=7 starts) and low quality (<7 stars)
+# to do: 
+# the following sensitivity analyses 
+# for prevalence of: high qualiy studies only (>=7 starts) and low quality (<7 stars)
 #
-#for prevalence in those 21 studies that reported vdd under our set threshold <20ng/ml
+# for prevalence in those 21 studies that reported vdd under our set threshold <20ng/ml
 #
-#for sample size of study >=150 versus <150 
+# for sample size of study >=150 versus <150 
 #
-#for papers that have common assay methods?! this was not done before so check 
+# for papers that have common assay methods?! this was not done before so check 
 
